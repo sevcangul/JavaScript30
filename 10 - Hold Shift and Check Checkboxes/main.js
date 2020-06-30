7dfaa4c96726ff18658253eac2f1d653065a46de
Sevcan <sevcansuperpeer@gmail.com>
2020-06-30 21:47:37 +0300

    Modify and fix the 10 - Hold Shift and Check Checkboxes files

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -7,6 +7,7 @@ function handleCheck(e) {
   // Check if they had the shift key down
   // AND check that they are checking it
   let inBetween = false;
+  
   if (e.shiftKey && this.checked) {
     // go ahead and do what we please
     // loop over every single checkbox
